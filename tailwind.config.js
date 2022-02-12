bcd066f952fef3a4832aab331f587d798d5cc11f
cw118 <carolyn94118@gmail.com>
2022-02-12 16:17:30 -0500

    Initial homepage, Tailwind config and images

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./en/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'midnight': '#69306D',
+      'dark-midnight': '#441f47',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
