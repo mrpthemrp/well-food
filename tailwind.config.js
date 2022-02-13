50f06a66f647e8102b617c3058b252dc0494eb0e
cw118 <carolyn94118@gmail.com>
2022-02-13 01:16:52 -0500

    Minor fixes for FAQ page and small typos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       'midnight': '#69306D',
       'dark-midnight': '#441f47',
       'russian-violet': '#0E103D',
-      'antique-fuschia': '#A5668B',
+      'antique-fuchsia': '#A5668B',
       'pink-lace': '#F2D7EE',
       'silver-pink': '#D3BCC0',
       'dark-silver-pink': '#B89399',
